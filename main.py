49eef347528c46039438fdf0b8dc82627dfcc88f
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-04-17 19:53:19 +0600

    Screen clear - added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from os_details import main as os_details
 from network_speed_monitor import main as network_speed_monitor
 
@@ -8,7 +10,8 @@ def main():
         print("\nMenu:")
         print("Input 1 to get OS details")
         print("Input 2 to Monitor Internet Speed")
-        print("x. Exit")
+        print("Press c to clear the screen")
+        print("Press x to exit\n")
 
         choice = input("\nEnter your choice: ")
         print(f"\n")
@@ -17,11 +20,13 @@ def main():
             os_details.main()
         elif choice == "2":
             network_speed_monitor.main()
+        elif choice == "c":
+            os.system("clear")
         elif choice == "x":
             print("Exiting...")
             break
         else:
-            print("Invalid choice. Please enter 1, 2, or 3.")
+            print("Invalid choice.")
 
 if __name__ == "__main__":
     main()
--- network_speed_monitor/main.py
+++ network_speed_monitor/main.py
@@ -2,28 +2,22 @@ import speedtest
 import time
 
 
-def network_monitor_speed(interval_seconds=60):
+def get_network_speed_data(interval_seconds=60):
     st = speedtest.Speedtest()
 
     download_speed = st.download() / 1_000_000
     upload_speed = st.upload() / 1_000_000
 
-    print(f"Download Speed: {download_speed:.2f} Mbps | Upload Speed: {upload_speed:.2f} Mbps")
-    # while True:
-    #     download_speed = st.download() / 1_000_000
-    #     upload_speed = st.upload() / 1_000_000
-
-    #     print(
-    #         f"Download Speed: {download_speed:.2f} Mbps | Upload Speed: {upload_speed:.2f} Mbps")
-
-    #     time.sleep(interval_seconds)
+    # print(f"Download Speed: {download_speed:.2f} Mbps | Upload Speed: {upload_speed:.2f} Mbps")
+    return f"Download Speed: {download_speed:.2f} Mbps | Upload Speed: {upload_speed:.2f} Mbps"
 
 
 def main():
-    network_monitor_speed()
+    network_speed_data = None
+    network_speed_data = get_network_speed_data()
+    print(network_speed_data)
 
 
 
 if __name__ == "__main__":
-    # You can customize the interval_seconds parameter as needed (default is 60 seconds)
     main()
